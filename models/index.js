1fd116c56269fd099a0ce218b1cb1959ca491600
Sureshkumar2001 <iamsureshkumar2001@gmail.com>
2024-04-20 11:29:02 +0530

    Store Management

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+// const uri = "mongodb+srv://adminhamza:adminhamza123&@cluster0.pzcviot.mongodb.net/InventoryManagementApp?retryWrites=true&w=majority";
+const uri="mongodb+srv://iamsureshkumar2001:Suresh2001@cluster0.cp0vnz3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+function main() {
+    mongoose.connect(uri).then(() => {
+        console.log("Succesfull")
+    
+    }).catch((err) => {
+        console.log("Error: ", err)
+    })
+}
+
+module.exports = { main };
